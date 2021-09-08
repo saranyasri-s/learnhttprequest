ad4753756bbaed0cfccd98257b2b5fa4a5d7e6d1
Saranya Sri <dentist.apassionateprogrammer@gmail.com>
2021-09-08 16:14:37 +0530

    fetch is used to send http request to get data

--- src/components/MoviesList.js
+++ src/components/MoviesList.js
@@ -3,28 +3,36 @@ import Movies from "./Movies";
 import classes from "./MoviesList.module.css";
 const DUMMY_MOVIES = [
   { title: "anjaan", description: "thriller", id: "anjaan" },
-  { title: "anjaan", description: "thriller", id: "anjaan" },
-  { title: "anjaan", description: "thriller", id: "anjaan" },
-  { title: "anjaan", description: "thriller", id: "anjaan" },
-  { title: "anjaan", description: "thriller", id: "anjaan" },
-  { title: "anjaan", description: "thriller", id: "anjaan" },
-  { title: "anjaan", description: "thriller", id: "anjaan" },
-  { title: "anjaan", description: "thriller", id: "anjaan" },
-  { title: "anjaan", description: "thriller", id: "anjaan" },
 ];
 function MoviesList() {
-  const [moviesList, setMoviesList] = useState(DUMMY_MOVIES);
-
+  const [moviesList, setMoviesList] = useState([]);
+  const fetchMoviesListHandler = () => {
+    fetch("https://swapi.dev/api/films/")
+      .then((response) => {
+        console.log(response);
+        return response.json();
+      })
+      .then((data) => {
+        console.log(data.results);
+        setMoviesList(data.results);
+      })
+      .catch(() => {
+        console.log("error");
+      });
+  };
   return (
     <div className={classes.MoviesPage}>
       <div className={classes.MoviesPageBackGround}></div>
-      <section className={classes.FetchMovies}>FETCH MOVIES</section>
+      <section className={classes.FetchMovies}>
+        <button onClick={fetchMoviesListHandler}>FETCH MOVIES</button>
+      </section>
       <section className={classes.MoviesList}>
         <ul>
           {moviesList.map((movie) => (
             <Movies
+              key={movie.episode_id}
               title={movie.title}
-              description={movie.description}
+              description={movie.opening_crawl}
             ></Movies>
           ))}
         </ul>
