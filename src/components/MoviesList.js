ca2047485b7891b8644065c601bd26d9e5600a14
Saranya Sri <dentist.apassionateprogrammer@gmail.com>
2021-09-09 16:48:36 +0530

    added post method in fetch

--- src/components/MoviesList.js
+++ src/components/MoviesList.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, useRef } from "react";
 import Movies from "./Movies";
 import classes from "./MoviesList.module.css";
 const DUMMY_MOVIES = [
@@ -8,6 +8,9 @@ function MoviesList() {
   const [moviesList, setMoviesList] = useState([]);
   const [isLoading, setIsLoading] = useState(false);
   const [errorMessage, setErrorMessage] = useState(null);
+  const titleRef = useRef();
+  const descriptionRef = useRef();
+
   const fetchMoviesListHandler = async () => {
     setIsLoading(true);
     // fetch("https://swapi.dev/api/films/")
@@ -26,24 +29,66 @@ function MoviesList() {
     setErrorMessage(null);
     setIsLoading(true);
     try {
-      const response = await fetch("https://swapi.dev/api/films");
+      const response = await fetch(
+        "https://learn-http-4c347-default-rtdb.firebaseio.com/movies.json"
+      );
+      // const response = await fetch("https://swapi.dev/api/films");
       const data = await response.json();
       if (!response.ok) {
         throw new Error("something went wrong!");
       }
-      setMoviesList(data.results);
+      let transformedMoviesList = [];
+      for (let key in data) {
+        transformedMoviesList.push({
+          title: data[key].title,
+          id: key,
+          description: data[key].description,
+        });
+      }
+      setMoviesList(transformedMoviesList);
     } catch (error) {
-      console.log(error);
-      console.log(error.message);
+      setErrorMessage("something went wrong!");
     }
     setIsLoading(false);
-    setErrorMessage("something went wrong!");
+  };
+  const addNewMovieHandler = async () => {
+    const title = titleRef.current.value;
+    const description = descriptionRef.current.value;
+
+    const newMovie = { title: title, description: description };
+
+    const response = await fetch(
+      "https://learn-http-4c347-default-rtdb.firebaseio.com/movies.json",
+      {
+        method: "post",
+        body: JSON.stringify(newMovie),
+      }
+    );
+    const data = await response.json();
+    console.log(data);
   };
   return (
     <div className={classes.MoviesPage}>
       <div className={classes.MoviesPageBackGround}></div>
+      <section className={classes.newMovie}>
+        <div>
+          <p>title</p>
+          <input ref={titleRef} type="text"></input>
+        </div>
+        <div>
+          <p>description</p>
+          <input ref={descriptionRef} type="text"></input>
+        </div>
+        <div className={classes.buttonContainer}>
+          <button className={classes.button} onClick={addNewMovieHandler}>
+            Add new movie
+          </button>
+        </div>
+      </section>
       <section className={classes.FetchMovies}>
-        <button onClick={fetchMoviesListHandler}>FETCH MOVIES</button>
+        <button className={classes.button} onClick={fetchMoviesListHandler}>
+          Fetch movies
+        </button>
       </section>
       <section className={classes.MoviesList}>
         {isLoading && moviesList.length < 1 && <p>Loading Movies...</p>}
@@ -51,9 +96,9 @@ function MoviesList() {
         <ul>
           {moviesList.map((movie) => (
             <Movies
-              key={movie.episode_id}
+              key={movie.id}
               title={movie.title}
-              description={movie.opening_crawl}
+              description={movie.description}
             ></Movies>
           ))}
         </ul>
